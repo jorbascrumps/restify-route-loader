d712fb5ad07aa8ce4e720bdb58f9e9b016d5ff73
iamchristopher <chriswright@live.com>
2017-07-21 13:03:00 -0400

    Removed unused build deps

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,4 @@
-// import path from 'path';
 import babel from 'rollup-plugin-babel';
-import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 
 export default {
@@ -10,10 +8,6 @@ export default {
         babel({
             exclude: 'node_modules/**'
         }),
-        // resolve({
-        //     jsnext: true,
-        //     main: true
-        // }),
         commonjs()
     ],
     dest: 'dist.js'
