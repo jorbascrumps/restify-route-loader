7948a7c5227253eb038c6d80ecc81adee6e8c110
iamchristopher <chriswright@live.com>
2017-07-21 10:22:32 -0400

    Added build script with Rollup

---
+++ rollup.config.js
@@ -0,0 +1,20 @@
+// import path from 'path';
+import babel from 'rollup-plugin-babel';
+import resolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
+
+export default {
+    entry: 'lib/index.js',
+    format: 'cjs',
+    plugins: [
+        babel({
+            exclude: 'node_modules/**'
+        }),
+        // resolve({
+        //     jsnext: true,
+        //     main: true
+        // }),
+        commonjs()
+    ],
+    dest: 'dist.js'
+}
