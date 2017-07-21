971a0e2ebd6b16529f78c6ca78f961b0fa88cc6e
iamchristopher <chriswright@live.com>
2017-07-21 13:01:03 -0400

    Moved lib file to project root

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import resolve from 'rollup-plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
 
 export default {
-    entry: 'lib/index.js',
+    entry: 'index.js',
     format: 'cjs',
     plugins: [
         babel({
