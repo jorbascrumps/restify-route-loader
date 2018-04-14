11cddd6c49f7d0fc5bd7201f64a2ff806d0130fe
iamchristopher <chriswright@live.com>
2018-04-14 16:59:29 -0400

    Added test for missing export

--- test.js
+++ test.js
@@ -1,5 +1,6 @@
 const should = require('should');
 const restify = require('restify');
+const path = require('path');
 
 const module = require('./index').default;
 
@@ -12,5 +13,13 @@ describe('Route Loader', () => {
             () => server.use(module()),
             /^Error: server is a required option$/
         );
-    })
+    });
+
+    it('should not fail if file doesn\'t export', () => {
+        should.ok(
+            () => server.use(module(server, {
+                routes: path.join(__dirname, 'no-routes')
+            }))
+        );
+    });
 });
