aa1474062b3e5c1d8931562ce67d6a6c4d49da51
iamchristopher <chriswright@live.com>
2018-04-14 16:58:20 -0400

    Fixed require statement

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 const should = require('should');
 const restify = require('restify');
 
-const module = require('./');
+const module = require('./index').default;
 
 let server;
 describe('Route Loader', () => {
