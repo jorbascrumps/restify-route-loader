eca04995367fd630e9fbbe0ef9162877cdf18663
iamchristopher <chriswright@live.com>
2018-04-14 15:54:14 -0400

    Added initial test to require server instance

---
+++ test.js
@@ -0,0 +1,16 @@
+const should = require('should');
+const restify = require('restify');
+
+const module = require('./');
+
+let server;
+describe('Route Loader', () => {
+    beforeEach(() => server = restify.createServer());
+
+    it('should require a server', () => {
+        should.throws(
+            () => server.use(module()),
+            /^Error: server is a required option$/
+        );
+    })
+});
