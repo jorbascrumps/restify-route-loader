1cef2bdf3b9f53b7c43e5358e82b09a6bec07d3a
maxdub <m.dubinovsky@arcane.ws>
2020-10-29 12:07:26 -0400

    adjusting test case to not include random code :D

--- test/routes/controller/get.js
+++ test/routes/controller/get.js
@@ -1,8 +1,7 @@
 export default [
     {
         version: '1.0.0',
-        async controller(req, res, next) {
-            res.send('KPI API');
+        controller(req, res, next) {
         }
     }
 ];
